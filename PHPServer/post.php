3944d936202b670c21fa1057cb55779c1787cc1e
kentwelcome@gmail.com <kentwelcome@gmail.com@4944f53e-21b9-11df-8b29-c50f69cede6d>
2011-03-10 08:46:16 +0000

    use another way to access database

--- PHPServer/post.php
+++ PHPServer/post.php
@@ -9,59 +9,48 @@ require("match.php");
 require("IPChoose.php");
 
 
-$AskUrl	= $_POST["ASK_URL"];
-$User 	= $_POST["User"];
+$AskUrl = $_POST["ASK_URL"];
+$User   = $_POST["User"];
 $Passwd = $_POST["Passwd"];
 
 // id check
 $MD5_hash = md5($Passwd);
 
 // set domain name 
-if ( $AskUrl != "" ){ 
+if ( $AskUrl != "" ){
 	$question=$AskUrl;
 }else{
 	echo "no url<br>\n";
 	exit(0);
 }
 
+
+// read configure file from dependns.ini
+$Config = parse_ini_file("dependns.ini",true);
+
 // set resolver list
-$resolverList[] = "168.95.1.1";
-$resolverList[] = "168.95.192.1";
-$resolverList[] = "139.175.55.244";
-$resolverList[] = "139.175.252.16";
-$resolverList[] = "139.175.150.20";
-$resolverList[] = "139.175.10.20";
-$resolverList[] = "203.187.0.6";
-$resolverList[] = "203.133.1.8";
-$resolverList[] = "211.78.130.1";
-$resolverList[] = "211.78.130.2";
-$resolverList[] = "61.56.211.185";
-$resolverList[] = "211.78.215.200";
-$resolverList[] = "211.78.215.137";
-$resolverList[] = "210.200.211.193";
-$resolverList[] = "210.200.211.225";
-$resolverList[] = "203.79.224.10";
-$resolverList[] = "203.79.224.30";
+$tmp_array = $Config['ResolverList']['list'];
+for ( $i = 0 ; $i < count($tmp_array) ; $i++ ){
+	$resolverList[] = $tmp_array[$i];
+}
 
 // set History List
 $HistoryList = array();
 
 
 // send dns query
+if ( $Config['Configure']['Timeout'] ){
+	$timeout=$Config['Configure']['Timeout']; 
+} else {
+	$timeout=10;
+}
 $port	=53;
-$timeout=60;
 $udp	=true;
 $type	="A";
 $debug	= "";
 
 for ( $i = 0 ;$i < count($resolverList) ; $i++ ){
-	$query_ans[$i] = new DNSQuery($resolverList[$i],
-			$port,
-			$timeout,
-			$udp,
-			$debug); 
-
-
+	$query_ans[$i] = new DNSQuery($resolverList[$i],$port,$timeout,$udp,$debug); 
 	$resultList[$i] = $query_ans[$i]->Query($question,$type);
 	if ($query_ans[$i]->error){
 		echo"erroe!<br>\n";
@@ -69,7 +58,6 @@ for ( $i = 0 ;$i < count($resolverList) ; $i++ ){
 		$counter = 0;
 		for ( $j = 0 ; $j < $resultList[$i]->count ; $j++ ){
 			if ( $resultList[$i]->results[$j]->type == 1 ){
-				//echo "DNS".$i.": ".$resultList[$i]->results[$j]->data."<br>";
 				$counter++;
 			}
 		}
@@ -79,97 +67,125 @@ for ( $i = 0 ;$i < count($resolverList) ; $i++ ){
 
 // check history database
 // db link
-$link = mysql_connect("localhost", "dependns", "dependns@833");
-if ( mysql_select_db("dependns", $link) ){
-	// check user passwd 
+if ( $Config['DataBase'] ) { 
+	$DB_host = $Config['DataBase']['SQL_Server'];
+	$DB_ID   = $Config['DataBase']['SQL_ID'];
+	$DB_PWD  = $Config['DataBase']['SQL_PWD'];
+
+
+} else {
+	$DB_host = "localhost";
+	$DB_ID	 = "dependns";
+	$DB_PWD	 = "dependns@833";
+}
+$odbc_id = odbc_connect("dependns",$DB_ID,$DB_PWD);
+if ($odbc_id){
+	// check password
 	$sql_query = "select UserInfo.passwd from UserInfo where username = '".$User."';";
-	$result = mysql_query($sql_query);
+	$result = odbc_exec($odbc_id,$sql_query);
 	if ( $result ){
-		$row = mysql_fetch_row($result);
-		echo $row[0]."\n";	
-		if ( $row[0] != $MD5_hash ){
-			mysql_close();
+		$row = odbc_fetch_array($result);
+		echo $row['passwd']."\n";
+		if ( $row['passwd'] != $MD5_hash ){
+			odbc_close($odbc_id);
 			echo "error login\n| error <br>";
 			exit(0);
 		}
 	}
-	
 
 	// check table domain_id 
 	$sql_query = "SELECT id FROM domain_id WHERE domain_name = '".$question."';";
-	$result = mysql_query($sql_query);
+	//$result = mysql_query($sql_query);
+	$result = odbc_exec($odbc_id,$sql_query);
 	if ( !$result ){
 		echo "can not select from table domain_id<br>\n";
 	}else {
-		$row = mysql_fetch_row($result);
-		if ( $row[0] == null ){
-			//echo "empty<br>";
+		//$row = mysql_fetch_row($result);
+		$row = odbc_fetch_array($result);
+		if ( $row['id'] == null ){
+			//echo "empty<br>\n";
 			$sql_query = "INSERT INTO domain_id (id,domain_name) VALUES( NULL , '".$question."');";
 
-			mysql_query($sql_query);
+			//mysql_query($sql_query);
+			odbc_exec($odbc_id,$sql_query);
 
 			$sql_query = "SELECT id FROM domain_id WHERE domain_name = '".$question."';";     
-			mysql_query($sql_query);
-			$newId = mysql_result(mysql_query($sql_query),0);
-			$sql_query = "CREATE TABLE domain_".$newId."(`id` BIGINT( 255 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,`round` INT( 255 ) UNSIGNED NOT NULL,`ip` VARCHAR( 16 ) NOT NULL,`ttl` BIGINT( 255 ) UNSIGNED NOT NULL,`date_time` DATETIME NOT NULL,`resolverIP` VARCHAR( 16 ) NOT NULL)ENGINE = MYISAM";
-			//echo "create table domain_".$newId."<br>";
-			mysql_query($sql_query);
+			//mysql_query($sql_query);
+			$result = odbc_exec($odbc_id,$sql_query);
+			//$newId = mysql_result(mysql_query($sql_query),0);
+			$newId = odbc_result($result,1);
+
+			for ( $i = 0 ; $i < count($resolverList) ; $i++ ){
+				for ( $j = 0 ; $j < $resultList[$i]->count ; $j++ ){
+					if ( $resultList[$i]->results[$j]->type == 1 ){
+						$sql_query = "INSERT INTO domain_DB (domain_id,ip,resolver) VALUES('"
+							.$newId."','"
+							.$resultList[$i]->results[$j]->data."','"
+							.$resolverList[$i]."')";
+						odbc_exec($odbc_id,$sql_query);
+					}
+				}
+			}
+
 		}else{
-			//echo "insert resolveAns into domain_".$row[0]."<br>";
-			$id = $row[0];
+			//echo "insert resolveAns into domain_".$row['id']."<br>";
+			$id = $row['id'];
 			for ( $i = 0 ;$i < count($resolverList) ; $i++ ){
 				for ( $j = 0 ; 
-						$j < $resultList[$i]->count ; 
-						$j++ ) {
+				$j < $resultList[$i]->count ; 
+				$j++ ) {
 
 					if ( $resultList[$i]->results[$j]->type == 1 ){                              
-						$sql_query = "INSERT INTO domain_"
-							.$id.
-							"(id,ip,resolverIP) VALUES(NULL,'"
-							.$resultList[$i]->results[$j]->data.
-							"','"
-							.$resolverList[$i].
-							"');";
-						mysql_query($sql_query);
+						$sql_query = "select domain_id from domain_DB where domain_id = '".$id.
+							"' and ip = '".$resultList[$i]->results[$j]->data.
+							"' and resolver = '".$resolverList[$i]."';";
+						$result = odbc_exec($odbc_id,$sql_query);
+						$row = odbc_fetch_row($result);
+						// add new ip when resolve ip change
+						if ( $row == false ){	
+							$sql_query = "INSERT INTO domain_DB (domain_id,ip,resolver) VALUES('"
+								.$id."','"
+								.$resultList[$i]->results[$j]->data."','"
+								.$resolverList[$i]."')";
+							odbc_exec($odbc_id,$sql_query);
+						}
+						$sql_query = "UPDATE domain_DB SET counter = counter+1 where domain_id = '".$id.
+							"' and ip = '".$resultList[$i]->results[$j]->data.
+							"' and resolver = '".$resolverList[$i].
+							"';";
+						odbc_exec($odbc_id,$sql_query);
 					} 
 				}
 			}
 			// do query
-			$sql_query = "select ip , count(ip) from domain_$id group by ip;";
-			//echo $sql_query;
-			$result = mysql_query($sql_query);
+			//$sql_query = "select ip , count(ip) from domain_$id group by ip;";
+			$sql_query = "select ip , counter from domain_DB where domain_id = $id group by ip;";
+			$result = odbc_exec($odbc_id,$sql_query);
 			if ( $result ){
-				while ( $row=mysql_fetch_row($result) ){
-					$ip = $row[0];	
+				while ( $row=odbc_fetch_array($result) ){
+					$ip = $row['ip'];	
 					$bClass = getBClass($ip);
 					$flag = false;
 					for ( $i = 0 ; $i < count($HistoryList) ; $i++ ){
 						if ( $bClass == $HistoryList[$i]->getBClass() )	{
-							$HistoryList[$i]->addIP($ip,$row[1]);
+							$HistoryList[$i]->addIP($ip,$row['counter']);
 							$flag = true;
 							break;
 						}
 					}
 					if (!$flag){
-						$HistoryList[] = new Answer($bClass,$ip,$row[1]);
+						$HistoryList[] = new Answer($bClass,$ip,$row['counter']);
 					}
 				}
 			} else {
 				echo "bad!<br>\n";
 			}
-			// get number of history data
 		}
 	}
-}
-
-mysql_close();
-
-for ( $i = 0 ; $i < count($HistoryList) ; $i++ ){
-	//for ( $j = 0 ; $j < count($HistoryList[$i]->ipList) ; $j++ ){
-	//echo $HistoryList[$i]->ipList[$j]->ip." times: ".$HistoryList[$i]->ipList[$j]->count."<br>";
 	//}
-	//echo $HistoryList[$i]->getBClass()." times: ".$HistoryList[$i]->classCount."<br>";
+	odbc_close($odbc_id);
 }
+
 
 // do dependns algorithm
 echo "|\n";
